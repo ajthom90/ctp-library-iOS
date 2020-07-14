9335917f0a18dcb8600ecfd497a16c6ca619ea0c
ajthom90 <ajthom@aafi.local>
2020-07-14 12:17:11 -0500

    Initial Commit

---
+++ CovidKotlinNative/CovidKotlinNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidKotlinNativeApp.swift
+//  CovidKotlinNative
+//
+//  Created by ajthom90 on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct CovidKotlinNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
